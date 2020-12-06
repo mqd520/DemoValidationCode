1d44571bd05cd9bee1a3cbf3ed58aa8db5ebd730
leon <1506816370@qq.com>
2020-12-07 01:05:14 +0800

    1、创建项目

---
+++ Code/WebApplication1/Controllers/FirstController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+using ValidationCodeHelper;
+
+namespace WebApplication1.Controllers
+{
+    public class FirstController : Controller
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Code()
+        {
+            DrawValidationCode code = new DrawValidationCode();
+            code.FontMinSize = 26;
+            code.FontMaxSize = 30;
+
+            MemoryStream ms = new MemoryStream();
+            code.CreateImage(ms);
+            Session["code"] = code.ValidationCode;
+
+            Response.ContentType = "image/gif";
+            Response.BinaryWrite(ms.GetBuffer());
+
+            return new EmptyResult();
+        }
+    }
+}
